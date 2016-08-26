48ce5c964f8b1ea9eb85cee59ad3d3750c4af918
Josiane <Josiane@JOSI>
2016-08-26 11:01:14 -0300

    UpForGit

---
+++ exLista1.1/Jogador.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exLista1._1
+{
+    public class Jogador
+    {
+        public int forca, ataque, defesa, velocidade;
+        public string name;
+        public float dinheiro;
+
+        public Jogador(int forca, int ataque, int defesa, int velocidade, string name, float dinheiro)
+        {
+            this.forca = forca;
+            this.ataque = ataque;
+            this.defesa = defesa;
+            this.velocidade = velocidade;
+            this.name = name;
+            this.dinheiro = dinheiro;
+        }
+
+        public void Atacar()
+        {
+            Console.WriteLine(name + " Atacou! ");
+        }
+
+        public void Defender()
+        {
+            Console.WriteLine(name + " Defendeu!");
+        }
+
+
+    }
+
+
+}
+
--- exLista1.1/Program.cs
+++ exLista1.1/Program.cs
@@ -10,6 +10,63 @@ namespace exLista1._1
     {
         static void Main(string[] args)
         {
+            Jogador[] jogadores = new Jogador[3];
+            jogadores[0] = new Jogador(100, 100, 100, 100, "Kenny", 0);
+            jogadores[1] = new Jogador(90, 100, 80, 75, "Kyle", 1000);
+            jogadores[2] = new Jogador(97, 100, 87, 100, "Stan", 500);
+
+            Console.WriteLine(" Total de jogadores: " + jogadores.Length);
+
+            Inimigo[] inimigos = new Inimigo[3];
+            inimigos[0] = new Inimigo(100, 100, 100, 100, "Cartman", 1000);
+            inimigos[1] = new Inimigo(87, 78, 89, 70, "Butters", 890);
+            inimigos[2] = new Inimigo(67, 59, 90, 60, "Token", 400);
+
+            Console.WriteLine(" Total de inimigos: " + inimigos.Length);
+
+            Console.WriteLine(" Media da força dos jogadores: " + RetornaMForca(jogadores));
+            Console.WriteLine(" Media da força dos inimigos: " + RetornaMForca(inimigos));
+            Console.WriteLine(" Media de ataque dos jogadores:" + RetornaMAtaque(jogadores));
+
+            Console.ReadLine();
+        }
+
+        static float RetornaMForca(Jogador[] jogadores)
+        {
+            float media = 0;
+
+            for (int i = 0; i < jogadores.Length; i++)
+            {
+                media += jogadores[i].forca;
+            }
+
+            media /= jogadores.Length;
+            return media;
+        }
+
+        static float RetornaMForca(Inimigo[] inimigos)
+        {
+            float media = 0;
+            int contador = 0;
+            foreach (Inimigo inimigo in inimigos)
+            {
+                media += inimigo.forca;
+                contador++;
+            }
+
+            return media / contador;
+        }
+
+        static float RetornaMAtaque(Jogador[] jogadores)
+        {
+            float media = 0;
+            for (int i = 0; i < jogadores.Length; i++)
+            {
+                media += jogadores[i].ataque;
+
+            }
+            
+            return media/jogadores.Length;
         }
     }
 }
